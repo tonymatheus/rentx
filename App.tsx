3ab3bd7f96397f0d21b2a6dc892a596c9746876a
tonymatheus <tony.chaves14@gmail.com>
2022-04-21 06:43:05 -0300

    Creates the project structure and add Expo fonts

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Home } from "./src/screens/Home";
+import AppLoading from "expo-app-loading";
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return <Home />;
+}
