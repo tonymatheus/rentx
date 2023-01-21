fb54038453c66cf84534181f4eae67df9546a702
tonymatheus <tony.chaves14@gmail.com>
2023-01-21 15:18:45 -0300

    Chore:📝 Implement button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useTheme } from "styled-components";
+
+import { Container, Title } from "./styles";
+
+interface Props {
+  color?: string;
+  title: string;
+  onPress: () => void;
+}
+
+export function Button({ title, color, onPress }: Props) {
+  const theme = useTheme();
+  return (
+    <Container onPress={onPress} color={color ? color : theme.colors.main}>
+      <Title>{title}</Title>
+    </Container>
+  );
+}
