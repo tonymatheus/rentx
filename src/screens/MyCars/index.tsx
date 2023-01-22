d8dc0d25b3f4102a30f7e2bce03148cd328a7960
tonymatheus <tony.chaves14@gmail.com>
2023-01-22 13:37:31 -0300

    Chore:Starts myCars page

---
+++ src/screens/MyCars/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Text } from "./styles";
+
+export function MyCars() {
+  return (
+    <Container>
+      <Text>Teste</Text>
+    </Container>
+  );
+}
