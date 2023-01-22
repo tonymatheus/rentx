0135ea760bf56e2adcd5eb30f28fde55d9559091
tonymatheus <tony.chaves14@gmail.com>
2023-01-22 13:36:57 -0300

    Chore:Add floating button to Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import Logo from "../../assets/logo.svg";
 import { api } from "../../services/api";
 import { CarDTO } from "../../dtos/CarDTO";
 import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container, Header, TotalCars, HeaderContent, CarList } from "./styles";
+import {
+  Container,
+  Header,
+  TotalCars,
+  HeaderContent,
+  CarList,
+  MyCarsButton,
+} from "./styles";
 import { Car } from "../../components/Car";
 import {
   ParamListBase,
@@ -13,14 +21,21 @@ import {
   useNavigation,
 } from "@react-navigation/native";
 import { Load } from "../../components/Load";
+import { useTheme } from "styled-components";
 
 export function Home() {
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [load, setLoading] = useState(true);
   const navigation = useNavigation<NavigationProp>();
 
+  const theme = useTheme();
+
   const handleCarDetails = (car: CarDTO) => {
     navigation.navigate("CarDetails", { car });
+  };
+
+  const handleMyCars = () => {
+    navigation.navigate("MyCars");
   };
 
   useEffect(() => {
@@ -63,6 +78,9 @@ export function Home() {
           )}
         />
       )}
+      <MyCarsButton onPress={() => navigation.navigate("MyCars")}>
+        <Ionicons name='ios-car-sport' size={40} color={theme.colors.shape} />
+      </MyCarsButton>
     </Container>
   );
 }
