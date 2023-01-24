fb1163442427ac29900f97463519fb6a3dd6b302
tonymatheus <tony.chaves14@gmail.com>
2023-01-24 10:25:55 -0300

    Chore:Add loading to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,18 +2,36 @@ import React from "react";
 import { useTheme } from "styled-components";
 
 import { Container, Title } from "./styles";
+import { ActivityIndicator } from "react-native";
 
 interface Props {
   color?: string;
   title: string;
   onPress: () => void;
+  enabled?: boolean;
+  loading?: boolean;
 }
 
-export function Button({ title, color, onPress }: Props) {
+export function Button({
+  title,
+  color,
+  onPress,
+  enabled = true,
+  loading = false,
+}: Props) {
   const theme = useTheme();
   return (
-    <Container onPress={onPress} color={color ? color : theme.colors.main}>
-      <Title>{title}</Title>
+    <Container
+      onPress={onPress}
+      enabled={enabled}
+      color={color ? color : theme.colors.main}
+      style={{ opacity: enabled === false || loading === true ? 0.5 : 1 }}
+    >
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape} />
+      ) : (
+        <Title>{title}</Title>
+      )}
     </Container>
   );
 }
