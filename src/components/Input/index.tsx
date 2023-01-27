f1ea9db4e3a0f343e13814573ca45a3d1af8d027
tonymatheus <tony.chaves14@gmail.com>
2023-01-27 08:36:08 -0300

    Chore:Add Implement new input Component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+
+import { Container, IconContainer, InputText } from "./styles";
+import { useTheme } from "styled-components";
+import { TextInputProps } from "react-native";
+
+interface Props extends TextInputProps {
+  iconName: React.ComponentProps<typeof Feather>["name"];
+}
+
+export function Input({ iconName, ...rest }: Props) {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <IconContainer>
+        <Feather name={iconName} size={20} color={theme.colors.text_detail} />
+      </IconContainer>
+      <InputText {...rest} />
+    </Container>
+  );
+}
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,26 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+import { TextInput } from "react-native";
+
+export const Container = styled.View`
+  flex-direction: row;
+  margin-bottom: 5px;
+`;
+
+export const IconContainer = styled.View`
+  width: 56px;
+  height: 55px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+  margin-right: 2px;
+`;
+
+export const InputText = styled(TextInput)`
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+  flex: 1;
+  color: ${({ theme }) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(15)}px;
+  padding: 0 23px;
+`;
