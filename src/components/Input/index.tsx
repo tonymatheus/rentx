45e356e70c3af1f84546c8cf396e20990ba7cbf0
tonymatheus <tony.chaves14@gmail.com>
2023-01-27 15:00:05 -0300

    chore:Add changes in validation styles in input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Feather } from "@expo/vector-icons";
 
 import { Container, IconContainer, InputText } from "./styles";
 import { useTheme } from "styled-components";
-import { TextInputProps } from "react-native";
+import { Alert, TextInputProps } from "react-native";
 
 interface Props extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>["name"];
+  value?: string;
 }
 
-export function Input({ iconName, ...rest }: Props) {
+export function Input({ iconName, value, ...rest }: Props) {
   const theme = useTheme();
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setIsFocused(false);
+    setIsFilled(!!value);
+  };
 
   return (
     <Container>
-      <IconContainer>
-        <Feather name={iconName} size={20} color={theme.colors.text_detail} />
+      <IconContainer isFocused={isFocused}>
+        <Feather
+          name={iconName}
+          size={20}
+          color={
+            isFocused || isFilled ? theme.colors.main : theme.colors.text_detail
+          }
+        />
       </IconContainer>
-      <InputText {...rest} />
+      <InputText
+        isFocused={isFocused}
+        {...rest}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+      />
     </Container>
   );
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,26 +1,46 @@
 import { RFValue } from "react-native-responsive-fontsize";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { TextInput } from "react-native";
+
+interface Props {
+  isFocused: boolean;
+}
 
 export const Container = styled.View`
   flex-direction: row;
   margin-bottom: 5px;
+  border-bottom-with: 2px;
+  border-bottom-color: transparent;
 `;
 
-export const IconContainer = styled.View`
+export const IconContainer = styled.View<Props>`
   width: 56px;
   height: 55px;
   align-items: center;
   justify-content: center;
   background-color: ${({ theme }) => theme.colors.background_secondary};
   margin-right: 2px;
+
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${({ theme }) => theme.colors.main};
+    `}
 `;
 
-export const InputText = styled(TextInput)`
+export const InputText = styled(TextInput)<Props>`
   background-color: ${({ theme }) => theme.colors.background_secondary};
   flex: 1;
   color: ${({ theme }) => theme.colors.text};
   font-family: ${({ theme }) => theme.fonts.primary_400};
   font-size: ${RFValue(15)}px;
   padding: 0 23px;
+
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${({ theme }) => theme.colors.main};
+    `}
 `;
