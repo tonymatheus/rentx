05fb06eaaa836ebba44f5eb3ff5802d9e7fd667e
tonymatheus <tony.chaves14@gmail.com>
2023-01-27 15:00:40 -0300

    chore:Change validation styles in PasswordInput

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -9,11 +9,24 @@ import { BorderlessButton } from "react-native-gesture-handler";
 interface Props extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>["name"];
   subIcon?: React.ComponentProps<typeof Feather>["name"];
+  value?: string;
 }
 
-export function PasswordInput({ iconName, subIcon, ...rest }: Props) {
-  const [isPasswordVisible, setIsPasswordVisible] = useState();
+export function PasswordInput({ iconName, subIcon, value, ...rest }: Props) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(true);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
   const theme = useTheme();
+
+  const handleInputFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setIsFocused(false);
+    setIsFilled(!!value);
+  };
 
   const handlePAsswordVisibilityChange = () => {
     setIsPasswordVisible((prevState) => !prevState);
@@ -21,12 +34,24 @@ export function PasswordInput({ iconName, subIcon, ...rest }: Props) {
 
   return (
     <Container>
-      <IconContainer>
-        <Feather name={iconName} size={20} color={theme.colors.text_detail} />
+      <IconContainer isFocused={isFocused} >
+        <Feather
+          name={iconName}
+          size={20}
+          color={
+            isFocused || isFilled ? theme.colors.main : theme.colors.text_detail
+          }
+        />
       </IconContainer>
-      <InputText {...rest} secureTextEntry={isPasswordVisible} />
+      <InputText
+        isFocused={isFocused}
+        {...rest}
+        secureTextEntry={isPasswordVisible}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+      />
       <TouchableWithoutFeedback onPress={handlePAsswordVisibilityChange}>
-        <IconContainer>
+        <IconContainer isFocused={isFocused}>
           <Feather
             name={isPasswordVisible ? "eye-off" : "eye"}
             size={20}
--- src/components/PasswordInput/styles.ts
+++ src/components/PasswordInput/styles.ts
@@ -1,25 +1,45 @@
 import { RFValue } from "react-native-responsive-fontsize";
-import styled from "styled-components/native";
-import { TextInput } from "react-native";
+import styled, { css } from "styled-components/native";
+import { TextInput, TextInputProps } from "react-native";
 
+interface Props {
+  isFocused: boolean;
+}
 export const Container = styled.View`
   flex-direction: row;
+
+  border-bottom-with: 2px;
+  border-bottom-color: transparent;
 `;
 
-export const IconContainer = styled.View`
+export const IconContainer = styled.View<Props>`
   width: 56px;
   height: 55px;
   align-items: center;
   justify-content: center;
   background-color: ${({ theme }) => theme.colors.background_secondary};
   margin-right: 2px;
+
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${({ theme }) => theme.colors.main};
+    `}
 `;
 
-export const InputText = styled(TextInput)`
+export const InputText = styled(TextInput)<Props>`
   background-color: ${({ theme }) => theme.colors.background_secondary};
   flex: 1;
   color: ${({ theme }) => theme.colors.text};
   font-family: ${({ theme }) => theme.fonts.primary_400};
   font-size: ${RFValue(15)}px;
   padding: 0 23px;
+
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${({ theme }) => theme.colors.main};
+    `}
 `;
