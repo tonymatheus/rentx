d99c33cb531f9bfb9344a308b2e541150f31dbba
tonymatheus <tony.chaves14@gmail.com>
2023-01-30 22:28:03 -0300

    chore:Update imageslider with bullet component

--- src/components/ImageSlader/index.tsx
+++ src/components/ImageSlader/index.tsx
@@ -1,13 +1,8 @@
 import React, { useRef, useState } from "react";
 
-import {
-  Container,
-  ImageIndex,
-  ImageIndexes,
-  CarImageWrapper,
-  CarImage,
-} from "./styles";
+import { Container, ImageIndexes, CarImageWrapper, CarImage } from "./styles";
 import { FlatList, ViewToken } from "react-native";
+import { Bullet } from "../Bullet";
 
 interface Props {
   imagesUrl: string[];
@@ -31,7 +26,7 @@ export const ImageSlider = ({ imagesUrl }: Props) => {
     <Container>
       <ImageIndexes>
         {imagesUrl.map((item, index) => (
-          <ImageIndex key={String(index)} active={index === imageIndex} />
+          <Bullet key={String(index)} active={index === imageIndex} />
         ))}
       </ImageIndexes>
       <FlatList
