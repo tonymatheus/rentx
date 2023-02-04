a8fad6fc9d7fda652098e1ac9e0d2d8603ad3ce9
tonymatheus <tony.chaves14@gmail.com>
2023-02-04 18:15:48 -0300

    chore:Add app provider

---
+++ src/hooks/index.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+
+import { AuthProvider } from "./auth";
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+const AppProvider = ({ children }: AppProviderProps) => {
+    return (
+        <AuthProvider>
+            {children}
+        </AuthProvider>
+    )
+};
+
+
+export {AppProvider}
